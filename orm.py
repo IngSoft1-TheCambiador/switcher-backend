9af4ce19b6049ef1c0edb1e40b1bec9b6b2e99f5
Fran <francisco.jose.navarro@mi.unc.edu.ar>
2024-09-16 21:02:13 -0300

    Add ORM entities with attributes

---
+++ orm.py
@@ -0,0 +1,34 @@
+from pony.orm import Database, PrimaryKey, Required, Set, Optional
+
+db = Database()
+
+class Shape(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    shape_type = Required(str)
+    is_blocked = Required(bool, default=False)
+    owner = Optional("Player", reverse="shapes")
+
+class Move(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    move_type = Required(str)
+    owner = Optional("Player", reverse="moves")
+
+class Player(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    game = Required("Game", reverse="players")
+    moves = Set("Move", reverse="owner")
+    shapes = Set(Shape, reverse="owner")
+
+class Game(db.Entity):
+    id = PrimaryKey(int, auto=True) 
+    name = Required(str)
+    min_players = Required(int, default=2, py_check=lambda x: x >= 2 and x <= 4)
+    max_players = Required(int, default=4, py_check=lambda x: x >= 2 and x <= 4)
+    is_init = Required(bool, default=False)
+    owner_id = Required(int)
+    current_player_id = Required(int)
+    players = Set(Player, reverse="game")
+
+db.bind("sqlite", "switcher_storage.sqlite", create_db=True)
+db.generate_mapping(create_tables=True)
