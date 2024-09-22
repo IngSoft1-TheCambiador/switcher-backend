335e54d02d833da33f42c5907a7c5dfde8296d5b
alvinary <49176889+alvinary@users.noreply.github.com>
2024-09-21 22:59:58 -0300

    Update test_websockets.py

--- test_websockets.py
+++ test_websockets.py
@@ -1,11 +1,15 @@
 from fastapi.testclient import TestClient
+from asyncio import run
 import main
 
-def test_websocket():
+async def test_websocket():
     client = TestClient(main.app)
     with client.websocket_connect("/ws/connect") as websocket:
         data = websocket.receive_json()
-        assert data == {"msg": "Hello WebSocket"}
-        assert main.manager.player_sockets != []
-        websocket.close()
-    assert main.manager.player_sockets == []
+        assert "new_id" in data.keys()
+        assert data["msg"] == "Hello WebSocket"
+        new_id = data["new_id"]
+        await main.manager.disconnect(new_id)
+    assert new_id not in main.manager.sockets_by_id
+
+run(test_websocket())
