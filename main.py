0522be160c0624b9d4c3f80cf391ab5af4a07974
Matteo Mariano <matteomariano10@mi.unc.edu.ar>
2024-10-20 01:31:49 -0300

    Fix bug

--- main.py
+++ main.py
@@ -224,8 +224,12 @@ async def leave_game(socket_id : int, game_id : int, player_id : int):
         elif (not game.is_init and game.owner_id == player_id):
             await manager.broadcast_in_game(game_id, "GAME CANCELLED BY OWNER")
             # unlink from the game all websockets remaining
-            for socket in manager.game_to_sockets[game_id]:
-                await manager.remove_from_game(socket, game_id)
+            sockets_in_game = manager.game_to_sockets[game_id].copy()
+            
+            for s in sockets_in_game:
+                print(f"manager.game_to_sockets[{game_id}]: {manager.game_to_sockets[game_id]}  (tomo socket {s}), socket_to_game: {manager.socket_to_game[s]}")
+                await manager.remove_from_game(s, game_id)
+
             game.cleanup()
 
         else:
