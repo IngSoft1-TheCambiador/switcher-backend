625abd9087192acaa96bda7e6c7c5b0ac5c0dd7f
santi <santiagolopezpereyra@gmail.com>
2024-11-11 20:04:53 -0300

    Added test for game creation with game deletion

--- test_game_creation.py
+++ test_game_creation.py
@@ -1,8 +1,10 @@
 import pytest
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, patch, Mock
 from fastapi.testclient import TestClient
+from orm import Game, Player, LogMessage
 from main import app, manager  
 from constants import *
+import datetime
 
 client = TestClient(app)
 
@@ -17,6 +19,12 @@ def mock_manager():
 def mock_player(mocker):
     mock_player = mocker.patch('main.Player')
     return mock_player
+
+
+@pytest.fixture 
+def mock_log_message(mocker):
+    mock_message = mocker.patch('main.LogMessage')
+    return mock_message
 
 @pytest.mark.asyncio
 @patch("main.Game")
@@ -169,3 +177,94 @@ async def test_invalid_password(mock_game_class, mock_manager, mock_player):
    
     
 
+@pytest.mark.asyncio
+@patch("main.Game")
+async def test_create_with_previous_games(mock_game_class, mock_manager, mock_player, mock_log_message):
+    # Mock data
+    socket_id = 1
+    game_name = "Test Game"
+    player_name = "Host"
+    password = "Apassword123"
+
+
+    log_msg = Mock(spec=LogMessage)
+    
+    player_a = mock_player.return_value
+
+    player_a.name = "A"
+    player_a.id = 1
+
+    game_1 = Mock(spec=Game)
+    game_2 = Mock(spec=Game)
+
+    player_b = Mock(spec=Player)
+    player_b.id = 123123
+    player_b.name = "B"
+
+
+    # A game with player_a
+    game_1.current_player = player_a
+    game_1.id = 100
+    game_1.owner_id = player_b.id
+    game_1.players = [player_a, player_b]
+    game_1.max_players = 4
+    game_1.is_init = True
+
+    # A game without player_a
+    game_2.current_player = player_a
+    game_2.id = 101
+    game_2.owner_id = player_a.id
+    game_2.players = [player_a, player_b]
+    game_2.max_players = 4
+    game_2.is_init = False
+    game_2.password = ""
+
+    log_msg.content = "Abandono partida"
+    log_msg.game = game_1.id,
+    log_msg.timestamp = datetime.datetime.now()
+    mock_log_message.return_value = log_msg
+
+
+    mock_player.get.return_value = player_a
+    mock_game_class.select.return_value = [game_1, game_2]
+
+
+    game_2.create_player.return_value = 123
+
+    mock_game_instance = mock_game_class.return_value
+    mock_game_instance.id = 42
+    mock_player_instance = mock_player.return_value
+    mock_player_instance.id = 1
+
+
+    assert len(game_1.players) == 2
+    assert len(game_2.players) == 2
+
+    def mock_create_player(player_name):
+        player = mock_player_instance
+        if len(mock_game_instance.players) == 0:
+            mock_game_instance.owner_id = player.id
+        mock_game_instance.players.add(player)
+        return player.id
+
+    mock_game_instance.create_player.side_effect = mock_create_player
+
+    
+    # Call the endpoint
+    response = client.put(
+        f"/create_game?socket_id={socket_id}&game_name={game_name}&player_name={player_name}&password={password}"
+    )
+    
+    assert response.status_code == 200
+    response_data = response.json()
+   
+    assert response_data['game_id'] == mock_game_instance.id  
+    assert response_data['player_id'] == 1  
+    assert response_data['password'] == password 
+    assert len(game_1.players) == 1
+    assert len(game_2.players) == 1
+    assert player_a not in game_1.players
+    assert player_a not in game_2.players
+    
+    # Assert that the WebSocket manager's add_to_game was called with the correct args
+    mock_manager.assert_called_once_with(socket_id, 42)
